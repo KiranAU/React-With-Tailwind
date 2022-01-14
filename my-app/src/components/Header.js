b6d41fb781b543c70d57caabc170e0207f264046
Kiran Uttarkar <kiran.uttarkar@gmail.com>
2022-01-14 09:21:28 -0500

    added products and services

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -138,17 +138,24 @@ export default function Header() {
           </div>
         </div>
       </nav>
-
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+          <h1 className="text-3xl font-bold text-gray-900">Our Products and Services</h1>
         </div>
       </header>
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* <!-- Replace with your content --> */}
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 text-lg">
+            <ul>
+            <li>Web Development</li>
+            <li>Mobile Development</li>
+            <li>UI/UX Design</li>
+            <li>Digital Marketing</li>
+            <li>Business Consulting</li>
+          </ul>
+            </div>
           </div>
           {/* <!-- /End replace --> */}
         </div>
